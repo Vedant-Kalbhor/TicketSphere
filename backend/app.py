1cbd1481c0947e083f644bec695bedefa2b3aa92
Vedant-Kalbhor <vedantkalbhor2005@gmail.com>
2026-02-12 19:49:38 +0530

    ec2

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.events import events_bp
 from routes.booking import booking_bp
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(events_bp)
 app.register_blueprint(booking_bp)
